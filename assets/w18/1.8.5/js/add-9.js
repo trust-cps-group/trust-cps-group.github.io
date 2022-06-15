295ebab215288259103ce0b0439983fd66c6afcd
eziobartocci80 <ezio.bartocci@tuwien.ac.at>
2022-06-15 22:49:49 +0200

    update

--- assets/w18/1.8.5/js/add-9.js
+++
@@ -1,9 +0,0 @@
-var _paq = window._paq = window._paq || [];
-_paq.push(["trackPageView"]), _paq.push(["enableLinkTracking"]), _paq.push(["enableHeartBeatTimer", 10]),
-    function() {
-        t = "https://purple.zkk.tuwien.ac.at:8080/", _paq.push(["setTrackerUrl", t + "add"]), _paq.push(["setSiteId", 9]);
-        var t, n = document,
-            e = n.createElement("script"),
-            s = n.getElementsByTagName("script")[0];
-        e.type = "text/javascript", e.async = !0, e.defer = !0, e.src = t + "add.js", s.parentNode.insertBefore(e, s)
-    }()
